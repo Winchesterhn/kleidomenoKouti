c793915d54456bc9741e0c2d300f2c799b08e8db
Winchesterhn <joan13sn@gmail.com>
2021-04-22 01:08:33 +0300

    Update kleidomenoKouti.ino
    it works but it does not reset after specific time. That way they might have press some of the fake buttons and still solve the riddle. I need to make it reset after 10 seconds.

--- kleidomenoKouti.ino
+++ kleidomenoKouti.ino
@@ -22,67 +22,93 @@ void setup() {
 void loop() {
 
   if (solved == 0) {
-    while (buttonState[0] < 800) {
-      buttonState[0] = analogRead(buttonsAndCode[0]);
-      Serial.print(buttonState[0]);
-      delay(2000);
-      if (buttonState[0] >= 800) {
-        Serial.print("first pressed \n");
+
+    for (int i = 0; i < 4; i++) {
+
+      unsigned long time_start = millis();// start time
+      while (millis() - time_start <= 6000) {// Gives them 6 seconds to press the next correct button
+
+        untilCorrectPressed(i);
+
       }
 
     }
-    while (buttonState[1] < 800) {
-      buttonState[1] = analogRead(buttonsAndCode[1]);
-      Serial.print(buttonState[1]);
-      delay(3000);
-      if (buttonState[1] >= 800) {
-        Serial.print("second pressed \n");
-      }
-
-    }
-    while (buttonState[2] < 800) {
-      buttonState[2] = analogRead(buttonsAndCode[2]);
-      Serial.print(buttonState[2]);
-      delay(3000);
-      if (buttonState[2] >= 800) {
-        Serial.print("third pressed \n");
-      }
-
-    }
-    while (buttonState[3] < 800) {
-      buttonState[3] = analogRead(buttonsAndCode[3]);
-      Serial.print(buttonState[3]);
-      delay(3000);
-      if (buttonState[3] >= 800) {
-        Serial.print("fourth pressed \n");
-      }
-    }
+    //    while (buttonState[0] < 800) {
+    //      buttonState[0] = analogRead(buttonsAndCode[0]);
+    //      Serial.print(buttonState[0]);
+    //      delay(2000);
+    //      if (buttonState[0] >= 800) {
+    //        Serial.print("first pressed \n");
+    //if((buttonState[1] > 800) || (buttonState[2] > 800) || (buttonState[3] > 800)){
+    //
+    //      }
+    //      }
+    //
+    //    }
+    //    while (buttonState[1] < 800) {
+    //      buttonState[1] = analogRead(buttonsAndCode[1]);
+    //      Serial.print(buttonState[1]);
+    //      delay(3000);
+    //      if (buttonState[1] >= 800) {
+    //        Serial.print("second pressed \n");
+    //if((buttonState[0] > 800) || (buttonState[2] > 800) || (buttonState[3] > 800)){
+    //
+    //      }
+    //      }
+    //
+    //    }
+    //    while (buttonState[2] < 800) {
+    //      buttonState[2] = analogRead(buttonsAndCode[2]);
+    //      Serial.print(buttonState[2]);
+    //      delay(3000);
+    //      if (buttonState[2] >= 800) {
+    //        Serial.print("third pressed \n");
+    //if((buttonState[0] > 800) || (buttonState[1] > 800) || (buttonState[3] > 800)){
+    //
+    //      }
+    //      }
+    //
+    //    }
+    //    while (buttonState[3] < 800) {
+    //      buttonState[3] = analogRead(buttonsAndCode[3]);
+    //      Serial.print(buttonState[3]);
+    //      delay(3000);
+    //      if (buttonState[3] >= 800) {
+    //        Serial.print("fourth pressed \n");
+    //if((buttonState[0] > 800) || (buttonState[1] > 800) || (buttonState[2] > 800)){
+    //
+    //      }
+    //      }
+    //    }
   }
   for (int i = 0; i < 4; i++) {
     Serial.print(buttonState[i]);
     Serial.print("\n");
   }
-  if ((buttonState[0] > 800 && buttonState[1] > 800 && buttonState[2] > 800 && buttonState[3] > 800)) {
+  if ((buttonState[0] > 800) && (buttonState[1] > 800) && (buttonState[2] > 800) && (buttonState[3] > 800)) {//  Why does it always return true????
 
     solved = 1;
   }
 
-  if (solved == 1); {
+  if (solved == 1); {//enter if the correct sequence was pressed
     // boxLock.write(180);// Make servo go to 90 degrees, unlocks the box.
     delay(5000);
     Serial.print("the box unlocks\n");
     Door();
   }
 }
-
+void untilCorrectPressed(int i) {
+  while (buttonState[i] < 800) {
+    buttonState[i] = analogRead(buttonsAndCode[i]);
+    Serial.print(buttonState[i]);//shows the value of its button. Helps in debugging
+    //    Serial.print("\n");
+    delay(2000);
+    if (buttonState[i] >= 800) {
+      Serial.print("pressed correctly \n");
+    }
+  }
+}
 void Door() {
-  digitalWrite(nDoor, HIGH);
+  digitalWrite(nDoor, HIGH);//Opens the box and waits until reset.
   while (true);
 }
-
-//unsigned long time_start = millis();// start time
-//while (millis() - time_start <= 3000) {// Gives them 3 seconds to press the next correct button
-//
-//  CorrectConnection[i] = testConnection(i);
-//
-//}
